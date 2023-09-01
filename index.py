2e7a49337fd42f19f2cf2def1d2adb15425035dd
Evans-mutuku <evansnyamai98@gmail.com>
2023-09-01 20:29:46 +0300

    Firts Commit

---
+++ index.py
@@ -0,0 +1,28 @@
+number = 10
+
+a,b,c = 5, 7, "Hello world"
+
+site1 = site2 = "Power Learn Project"
+
+num1 = 5
+print(num1)
+
+
+num2 = 8.5
+num3 = 1+2j
+
+name = "My name is PLP"
+
+
+print("Evans Mutuku")
+
+
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+print(languages[1])
+
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+print(products[2])
