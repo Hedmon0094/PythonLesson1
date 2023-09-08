2529a6b206adbd4030822a0370ea6b2b8cf7aa84
Evans-mutuku <evansnyamai98@gmail.com>
2023-09-08 20:21:53 +0300

    Lesson 2

---
+++ sets.py
@@ -0,0 +1,71 @@
+student_ids = {112, 114, 117, 113}
+
+print(student_ids)
+
+
+# Key/Value Pair
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+
+print(capital_city)
+
+
+a = 13
+b = 5
+
+# +, *, /, - , %
+''' Basic Math Operations'''
+
+
+print("Sum", a + b)
+print("Mult", a * b)
+print("Div", a / b)
+print("Sub", a - b)
+
+
+print("Remainder", a % b)
+
+
+# Assignment Operators
+
+i = 10
+t = 5
+
+
+i += t
+
+print(i)
+
+
+x = 12 
+z = 9
+
+print(x > z)
+
+y = 7
+v = 12
+
+print((y > 5) and (v >= 6))
+
+
+
+x1 = 5
+y1 = 5
+
+x2 = [1,2,3]
+y2 = [1,2,3]
+
+x3 = "Hello"
+y3 = "Hello"
+
+print(x1 is not y1)
+print(x2 is y2)
+print(x3 is y3)
+
+
+t1 = "Hello world"
+t2 = {1: "Power", 2: "Learn"}
+
+
+print("H" in t1)
+print("a" in t2)
+print(1 in t2)
