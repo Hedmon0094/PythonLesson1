7965d8ab329216e1b3f77f38d15907c6ebbf6b37
Evans-mutuku <evansnyamai98@gmail.com>
2023-09-15 21:08:05 +0300

    doing conditional statements

---
+++ conditionals.py
@@ -0,0 +1,48 @@
+# if condition:
+# Print Output
+
+
+
+# hungry = True
+
+# if hungry:
+#     print("Eat Some Food")
+
+
+
+# num = 43
+
+# if num >= 32:
+#     print("The Value of Num is greater than 32")
+# else:
+#     print("Num is less than 32")
+    
+    
+
+# marks = 67
+
+# if marks >= 90:
+#     print("Scored an A")
+# elif marks >= 80:
+#     print("Scored a B")
+# elif marks >= 70:
+#     print("Scored a C")
+# else:
+#     print("Failed")
+
+
+
+names = ["Habibi", "Lucy", "George", "Wendy"]
+
+for name in names:
+    print(name)
+
+#print(names)
+
+
+
+welcome_message = "Welcome to PLP"
+
+for i in range(5):
+    print(welcome_message)
+
